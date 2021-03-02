8a09a051740c7c9b9df2de017f5fcdc543b3d572
Zondazx <mzxgd16@gmail.com>
2021-03-02 17:22:37 -0400

    feat: konami code working

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const title = document.querySelector('.header');
+const authorText = document.querySelector('.author');
 const pressed = [];
+
 const secretCode = 'pekopeko';
+const audio = new Audio('assets/sounds/konpeko.mp3');
 
 window.addEventListener('keyup', e => {
   pressed.push(e.key);
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
   if (pressed.join('').includes(secretCode)) {
     title.textContent = 'Haha, you found the code!';
+    authorText.textContent = 'AH↓HA↑HA↑HA↑HA↑';
+
+    audio.currentTime = 0;
+
+    audio.play();
 
     setTimeout(() => {
       title.textContent = 'Hello. 👋';
-    }, 5000);
+      authorText.textContent = 'My name is Manuel Zapata.'
+    }, 10000);
   }
 });
