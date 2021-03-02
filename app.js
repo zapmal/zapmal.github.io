90d25eefe8241b8bbb6a322b1ba7633c9668521c
Zondazx <mzxgd16@gmail.com>
2021-03-02 13:59:13 -0400

    feat: better design, rephrase some stuff, remove images

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-const image = document.querySelector('.banner');
 const title = document.querySelector('.header');
 const pressed = [];
-const secretCode = 'pekopeko';
+const secretCode = 'ameliabestgirl';
 
 window.addEventListener('keyup', e => {
   pressed.push(e.key);
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
   if (pressed.join('').includes(secretCode)) {
-    image.src = 'assets/images/pekolaugh.jpg';
-    title.textContent = 'Nice!';
+    title.textContent = 'Haha, you found the code!';
 
     setTimeout(() => {
-      image.src = 'assets/images/banner.jpg';
       title.textContent = 'Hello. 👋';
     }, 5000);
   }
